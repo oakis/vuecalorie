da21ecb07a9211b60e1db06c427896521b40e3df
Anders Ekman <oakismen@gmail.com>
2019-09-26 12:48:17 +0200

    add header and nav

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <header></header>
+    <Header />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Header
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0 25px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <h1>Vue Calorie App</h1>
+    <nav>
+      <a href>Hitta recept</a>
+      <a href>Min sida</a>
+      <a href>Om VCA</a>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto;
+  background-color: green;
+  padding: 25px;
+  h1 {
+    font-size: 3em;
+    margin: 0;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    a {
+      font-size: 1.5em;
+      color: #111;
+      margin: 0 10px;
+      text-decoration: none;
+    }
+  }
+}
+</style>
