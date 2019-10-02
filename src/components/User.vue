ed83500b7a0feb5c0bf4bf6cfbf4fac15b1f106c
unknown <oakismen@gmail.com>
2019-10-02 14:26:04 +0200

    start working on create recipe

---
+++ src/components/User.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="user">
+    <h2>"Username placeholder"</h2>
+    <Input placeholder="Hitta ingrediens" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Input from "./shared/Input.vue";
+
+@Component({
+  components: {
+    Input
+  }
+})
+export default class User extends Vue {}
+</script>
+
+<style scoped lang="scss">
+#user {
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+  background-color: purple;
+  p {
+    margin: 0;
+  }
+  ul {
+    margin: 0;
+    padding-left: 20px;
+  }
+}
+</style>
---
+++ src/components/shared/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <input :placeholder="this.placeholder" @input="onInput" v-model="inputValue" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+@Component
+export default class Input extends Vue {
+  @Prop(String) placeholder: string = "";
+  inputValue: String = "";
+
+  async onInput() {
+    const ingredients = await axios.post(
+      "http://localhost:4000/ingredients/search",
+      {
+        search: this.inputValue
+      }
+    );
+    console.log(ingredients.data);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
