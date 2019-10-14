205c80d5e18d5bb6d6319638ec418be44538287f
Anders Ekman <oakismen@gmail.com>
2019-10-14 09:35:46 +0200

    add Roboto

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,10 @@ body {
   color: #111;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding: 0 25px;
+  margin: 0 25px;
   main {
     padding: 25px;
   }
