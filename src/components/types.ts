7b788ec43c4810cbee27caddac299c6553676571
unknown <oakismen@gmail.com>
2019-10-15 09:48:56 +0200

    fix linting

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,20 +1,20 @@
 export interface IIngredient {
-    readonly _id: string;
-    name: string;
-    kcal: number;
-    readonly __v: number;
+  readonly _id: string;
+  name: string;
+  kcal: number;
+  readonly __v: number;
 }
 
 export interface IRecipe {
-    readonly _id: string;
-    name: string;
-    ingredients: Array<IIngredient>;
-    readonly __v: number;
+  readonly _id: string;
+  name: string;
+  ingredients: Array<IIngredient>;
+  readonly __v: number;
 }
 
 export interface IUser {
-    readonly _id: string;
-    username: string;
-    email: string;
-    token: string;
+  readonly _id: string;
+  username: string;
+  email: string;
+  token: string;
 }
